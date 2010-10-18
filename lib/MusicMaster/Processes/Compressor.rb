ab6d8c7661b9d5b0a2cb541c6973fb2682228946
Muriel Salvan <MurielSalvan@users.sourceforge.net>
2010-10-18 18:18:29 +0000

    * Compressor: Implemented Attack and Release specifications. * Bug correction: Compressor: Specifying DB units did not work correctly.
    git-svn-id: https://musicmaster.svn.sourceforge.net/svnroot/musicmaster@8 9041aa18-91c9-4662-906d-dbed61b9fc88

--- lib/MusicMaster/Processes/Compressor.rb
+++ lib/MusicMaster/Processes/Compressor.rb
@@ -1,18 +1,24 @@
+require 'WSK/Common'
+
 module MusicMaster
 
   module Processes
 
     class Compressor
       
-      # Minimal value represented in DB
-      MINIMAL_DB = -1.0/0.0
+      include WSK::Common
+
+      # -Infinity
+      MINUS_INFINITY = -1.0/0.0
 
       # Parameters of this process:
       # * *:DBUnits* (_Boolean_): Are units in DB format ? [optional = false]
-      # * *:Threshold* (_Float_): The threshold below which there is no compression (in DB if :DBUnit is true)
-      # * *:Ratio* (_Float_): Compression ratio to apply above threshold
-      # * *:Attack* (_String_): The attack duration (either in seconds or in samples)
-      # * *:Release* (_String_): The release duration (either in seconds or in samples)
+      # * *:Threshold* (_Float_): The threshold below which there is no compression (in DB if :DBUnit is true, else in a [0..1] scale)
+      # * *:Ratio* (_Float_): Compression ratio to apply above threshold.
+      # * *:AttackDuration* (_String_): The attack duration (either in seconds or in samples)
+      # * *:AttackDamping* (_String_): The attack damping value in the duration previously defined (in DB if :DBUnit is true, else in a [0..1] scale). The compressor will never attack more than :AttackDamping values during a duration of :AttackDuration.
+      # * *:ReleaseDuration* (_String_): The release duration (either in seconds or in samples)
+      # * *:ReleaseDamping* (_String_): The release damping value in the duration previously defined (in DB if :DBUnit is true, else in a [0..1] scale). The compressor will never release more than :ReleaseDamping values during a duration of :ReleaseDuration.
 
       # Execute the process
       #
@@ -48,7 +54,7 @@ module MusicMaster
         end
         # Ratio must be > 1
         if (iParams[:Ratio] <= 1)
-          logErr "Ratio (#{iParams[:Threshold]}) has to be > 1"
+          logErr "Ratio (#{iParams[:Ratio]}) has to be > 1"
           lParamsOK = false
         end
         if (lParamsOK)
@@ -63,16 +69,29 @@ module MusicMaster
             File::unlink(lTempWaveFile)
           end
 
+          # Get the file header for various measures later
+          lHeader = nil
+          File.open(iInputFileName, 'rb') do |iFile|
+            lError, lHeader = readHeader(iFile)
+            if (lError != nil)
+              logErr "An error occurred while reading header: #{lError}"
+            end
+          end
+
           # Create the Compressor's function based on the parameters
-          require 'WSK/Common'
+          # Minimal value represented in DB.
+          # This value will be used to replace -Infinity
+          lMinimalDBValue = nil
           lCompressorFunction = WSK::Functions::Function.new
           if (lDBUnits)
+            # The minimal DB value is the smallest ratio possible for RMS values of this file (1/2^(BPS-1)) converted in DB and minus 1 to not mix it with the ratio 1/2^(BPS-1)
+            lMinimalDBValue = val2db(1, 2**(lHeader.NbrBitsPerSample-1))[0] - 1
             lCompressorFunction.set( {
               :FunctionType => WSK::Functions::FCTTYPE_PIECEWISE_LINEAR,
               :Points => [
-                [MINIMAL_DB, MINIMAL_DB],
+                [lMinimalDBValue, lMinimalDBValue],
                 [iParams[:Threshold], iParams[:Threshold]],
-                [0, iParams[:Threshold] - iParams[:Threshold]/iParams[:Ratio] ]
+                [0, iParams[:Threshold] - Float(iParams[:Threshold])/iParams[:Ratio] ]
               ]
             } )
           else
@@ -81,10 +100,11 @@ module MusicMaster
               :Points => [
                 [0, 0],
                 [iParams[:Threshold], iParams[:Threshold]],
-                [1, iParams[:Threshold] + (1-iParams[:Threshold])/iParams[:Ratio] ]
+                [1, iParams[:Threshold] + (1.0-iParams[:Threshold])/iParams[:Ratio] ]
               ]
             } )
           end
+          logInfo "Compressor transfer function: #{lCompressorFunction.functionData[:Points].inspect}"
 
           # Compute the volume transformation function based on the profile function and the Compressor's parameters
           lTempVolTransformFile = "#{iTempDir}/#{File.basename(iInputFileName)[0..-5]}.VolumeFct.rb"
@@ -96,14 +116,20 @@ module MusicMaster
             lProfileFunction.readFromFile(lTempVolProfileFile)
             if (lDBUnits)
               # Convert the Profile function in DB units
-              lProfileFunction.convertToDB
+              lProfileFunction.convertToDB(1)
+              # Replace -Infinity with lMinimalDBValue
+              lProfileFunction.functionData[:Points].each do |ioPoint|
+                if (ioPoint[1] == MINUS_INFINITY)
+                  ioPoint[1] = lMinimalDBValue
+                end
+              end
             end
+
             # Clone the profile function before applying the map
             lNewProfileFunction = WSK::Functions::Function.new
             lNewProfileFunction.set(lProfileFunction.functionData.clone)
             
             # Transform the Profile function with the Compressor function
-            # TODO: Check if we really need Attack and Release
             lNewProfileFunction.applyMapFunction(lCompressorFunction)
 
             # The difference of the functions will give the volume transformation profile
@@ -116,6 +142,17 @@ module MusicMaster
               # The volume transformation will be a ratio
               lDiffProfileFunction.divideByFunction(lProfileFunction)
             end
+
+            # Apply damping for attack and release times
+            lAttackDuration = readDuration(iParams[:AttackDuration], lHeader.SampleRate)
+            lAttackSlope = Float(iParams[:AttackDamping])/Float(lAttackDuration)
+            lReleaseDuration = readDuration(iParams[:ReleaseDuration], lHeader.SampleRate)
+            lReleaseSlope = Float(iParams[:ReleaseDamping])/Float(lReleaseDuration)
+            if (lDBUnits)
+              lAttackSlope = -lAttackSlope
+              lReleaseSlope = -lReleaseSlope
+            end
+            lDiffProfileFunction.applyDamping(lReleaseSlope, -lAttackSlope)
 
             # Save the volume transformation file
             lDiffProfileFunction.writeToFile(lTempVolTransformFile)
